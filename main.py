84193693cf4ecc3a18865c17fe03bb946423a350
thegrimcoder <mbrust035@gmail.com>
2025-09-18 17:55:21 -0500

    Fiddling with location of grid

--- main.py
+++ main.py
@@ -1,29 +1,77 @@
 import pygame 
 from cell import *
 
-GRID_WIDTH = 100
-GRID_HEIGHT = 100
-CELL_SIZE = 10
+GRID_WIDTH = 25
+GRID_HEIGHT = 25
+CELL_SIZE = 20
+SCREEN_RATIO = 1
+SCREEN_WIDTH_OFFSET = 0
+SCREEN_HEIGHT_OFFSET = 0
+
+def draw_grid(screen, grid):
+    #Clear Screen
+    screen.fill((0, 0, 0))
+
+    #Iterate through the grid
+    for x in range(GRID_WIDTH):
+        for y in range(GRID_HEIGHT):
+            cell = grid[x][y]
+
+            #Checking if the Cell is alive - obtaining appropriate color
+            if cell.get_alive():
+                color = cell.get_color()
+            else:
+                color = (255, 255, 255)
+
+            #Calculate cell position based on screen size
+            rect = pygame.Rect(x*CELL_SIZE*SCREEN_RATIO+SCREEN_WIDTH_OFFSET, y*CELL_SIZE*SCREEN_RATIO+SCREEN_HEIGHT_OFFSET, CELL_SIZE*SCREEN_RATIO, CELL_SIZE*SCREEN_RATIO)
+            #rect = pygame.Rect(x*CELL_SIZE*SCREEN_RATIO+SCREEN_WIDTH_OFFSET, y*CELL_SIZE*SCREEN_RATIO+SCREEN_HEIGHT_OFFSET, CELL_SIZE*SCREEN_RATIO+SCREEN_WIDTH_OFFSET, CELL_SIZE*SCREEN_RATIO+SCREEN_HEIGHT_OFFSET)
+
+            #Draw the cell
+            pygame.draw.rect(screen, color, rect)
+
+            #Draw a border around the cell
+            pygame.draw.rect(screen, (0,0,0), rect, 1)
 
 def main():
-    # Initialize pygame
+    #Initialize pygame
     pygame.init()
     time_clock = pygame.time.Clock()
 
-    # Screen Dimensions - Set to system display dimensions
+    #Screen Dimensions - Set to system display dimensions
     screen_info = pygame.display.Info()
+    print(screen_info)
     screen_width = screen_info.current_w
     screen_height = screen_info.current_h
-    screen = pygame.display.set_mode((screen_width, screen_height))
+
+    #Update screen ratio based on dynamic screen size
+    global SCREEN_RATIO
+    SCREEN_RATIO = screen_width/screen_height
+    
+    #Update screen offset based on dynamic screen size - centers grid
+    grid_screen_width = GRID_WIDTH*CELL_SIZE
+    grid_screen_height = GRID_HEIGHT*CELL_SIZE
+
+    global SCREEN_WIDTH_OFFSET
+    SCREEN_WIDTH_OFFSET = (screen_width-grid_screen_width)/2
+
+    global SCREEN_HEIGHT_OFFSET
+    SCREEN_HEIGHT_OFFSET = 10
+
+    screen = pygame.display.set_mode((screen_width, screen_height), pygame.SCALED)
     pygame.display.set_caption("Battle of Life")
 
     #Create starting grid
     grid = [[None for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
 
-    #Initialize all cells in the grid to Cells
-    for x in range(GRID_WIDTH-1):
-        for y in range(GRID_HEIGHT-1):
+    #Create a second grid store the next state of the game board
+    next_grid = [[None for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
+
+    #Initialize all cells in both grids to dead Cells
+    for x in range(GRID_WIDTH):
+        for y in range(GRID_HEIGHT):
             grid[x][y] = Cell(x, y, CELL_SIZE)
+            next_grid[x][y] = Cell(x, y, CELL_SIZE)
 
     # Game loop
     running = True
@@ -32,10 +80,8 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-        #Create a second grid store the next state of the game board
-        next_grid = [[None for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
-
-        
+        #Draw Grid
+        draw_grid(screen, next_grid)
 
         #Update the display
         pygame.display.flip()
@@ -45,3 +91,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+            
